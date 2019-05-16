d2d6a3a183530bde23526764208a950b1c87a04b
David Qorashi <davoud.qorashi@gmail.com>
2019-05-16 14:19:33 -0700

    Finish linkedlist chapter

--- epi_judge_python/int_as_list_add.py
+++ epi_judge_python/int_as_list_add.py
@@ -1,9 +1,24 @@
+from list_node import ListNode
 from test_framework import generic_test
-
 
 def add_two_numbers(L1, L2):
     # TODO - you fill in here.
-    return None
+    prev = prehead = ListNode()
+    carry = 0
+    while L1 or L2 or carry:
+    	new_node = ListNode()
+    	prev.next = new_node
+    	prev = prev.next
+    	val = (L1.data if L1 else 0) + (L2.data if L2 else 0) + carry
+    	if val < 10:
+    		new_node.data = val
+    		carry = 0
+    	else:
+    		new_node.data = val - 10
+    		carry = 1
+    	L1 = L1.next if L1 else None
+    	L2 = L2.next if L2 else None
+    return prehead.next
 
 
 if __name__ == '__main__':
