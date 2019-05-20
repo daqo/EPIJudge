d0b8f194a87da8b8a5d99c51c7ea103c3e3547a2
David Qorashi <davoud.qorashi@gmail.com>
2019-05-20 13:27:38 -0700

    Solve is_tree_height_balanced

--- epi_judge_python/is_tree_balanced.py
+++ epi_judge_python/is_tree_balanced.py
@@ -2,12 +2,24 @@ from test_framework import generic_test
 
 
 def is_balanced_binary_tree(tree):
-    # TODO - you fill in here.
-    return True
+		# TODO - you fill in here.
+		def is_balanced(tree):
+			if not tree: return (True, 0)
+			is_left_balanced, left_height = is_balanced(tree.left)
+			if not is_left_balanced:
+				return (False, -1)
+			is_right_balanced, right_height = is_balanced(tree.right)
+			if not is_right_balanced:
+				return (False, -1)
+			return (abs(left_height - right_height) <= 1, max(left_height, right_height) + 1)
+
+		return is_balanced(tree)[0]
+
+
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main("is_tree_balanced.py",
-                                       'is_tree_balanced.tsv',
-                                       is_balanced_binary_tree))
+		exit(
+				generic_test.generic_test_main("is_tree_balanced.py",
+																			 'is_tree_balanced.tsv',
+																			 is_balanced_binary_tree))
