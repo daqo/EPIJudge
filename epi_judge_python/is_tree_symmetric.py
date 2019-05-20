83aa3ffd1c0bc46c20236b037a2a5c4ce8ec68fb
David Qorashi <davoud.qorashi@gmail.com>
2019-05-20 14:29:12 -0700

    solve if tree is symmetric

--- epi_judge_python/is_tree_symmetric.py
+++ epi_judge_python/is_tree_symmetric.py
@@ -2,11 +2,22 @@ from test_framework import generic_test
 
 
 def is_symmetric(tree):
-    # TODO - you fill in here.
-    return True
+		def check(l_tree, r_tree):
+			if not l_tree and not r_tree: return True
+			if l_tree and not r_tree: return False
+			if not l_tree and r_tree: return False
+			if l_tree.data == r_tree.data:
+				if not check(l_tree.left, r_tree.right):
+					return False
+				if not check(l_tree.right, r_tree.left):
+					return False
+			else: return False
+			return True
+		if not tree: return True
+		return check(tree.left, tree.right)
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main("is_tree_symmetric.py",
-                                       'is_tree_symmetric.tsv', is_symmetric))
+		exit(
+				generic_test.generic_test_main("is_tree_symmetric.py",
+																			 'is_tree_symmetric.tsv', is_symmetric))
