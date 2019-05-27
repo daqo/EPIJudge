c40c28da9b587a0b3bede6d5abcca3849679ccc9
David Qorashi <davoud.qorashi@gmail.com>
2019-05-27 16:49:14 -0700

    Solve computing power of a number

--- epi_judge_python/power_x_y.py
+++ epi_judge_python/power_x_y.py
@@ -2,10 +2,18 @@ from test_framework import generic_test
 
 
 def power(x, y):
-    # TODO - you fill in here.
-    return 0.0
+		res = 1.0
+		if y < 0:
+			x = 1.0 / x
+			y = -y
+		while y:
+			if y & 1:
+				res = res * x
+			x = x * x
+			y = y >> 1
+		return res
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main("power_x_y.py", 'power_x_y.tsv', power))
+		exit(
+				generic_test.generic_test_main("power_x_y.py", 'power_x_y.tsv', power))
