9c055489b682d3a0009170bd63adb703ff6c7d2b
David Qorashi <davoud.qorashi@gmail.com>
2019-05-27 16:08:45 -0700

    Solve primitive divide problem

--- epi_judge_python/primitive_divide.py
+++ epi_judge_python/primitive_divide.py
@@ -1,12 +1,21 @@
 from test_framework import generic_test
 
 
-def divide(x, y):
-    # TODO - you fill in here.
-    return 0
+def divide(x, origy):
+		k, result = 0, 0
+		while x >= origy:
+			y = origy
+			k = 0
+			while x >= y:
+				prev = y
+				y <<= 1
+				k += 1
+			result += (1 << (k - 1))
+			x = x - prev
+		return result
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main("primitive_divide.py",
-                                       "primitive_divide.tsv", divide))
+		exit(
+				generic_test.generic_test_main("primitive_divide.py",
+																			 "primitive_divide.tsv", divide))
